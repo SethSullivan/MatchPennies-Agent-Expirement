e0b0757b546ab0ace201a04497b1add3e0f60940
SethSullivan <sethsull@udel.edu>
2023-05-16 11:06:36 -0400

    automatically set all attributes instead of calling function

--- Optimal_Model/Analysis/Group_Subject_Models_Object.py
+++ Optimal_Model/Analysis/Group_Subject_Models_Object.py
@@ -7,10 +7,7 @@ wheel = dv.ColorWheel()
 class Group_Subjects():
     def __init__(self,objects):
         self.objects = objects
-    def combine_all_subjects(self,metric):
-        '''
-        List comprehension into np array to put the subjects at index 0
-        '''
+    
         #* Loop through all attributes, and set the group attribute with all subjects combined
         for a in dir(self.objects[0]):
             if not a.startswith('__'):
