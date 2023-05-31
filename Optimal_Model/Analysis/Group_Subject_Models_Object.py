d325d64e37005d7318820d3f6634f63ae83bda8f
SethSullivan <sethsull@udel.edu>
2023-05-31 09:14:49 -0400

    passing dictionary of objects instead of list

--- Optimal_Model/Analysis/Group_Subject_Models_Object.py
+++ Optimal_Model/Analysis/Group_Subject_Models_Object.py
@@ -7,8 +7,8 @@ wheel = dv.ColorWheel()
 class Group_Subjects():
     def __init__(self,objects):
         self.objects = objects
-    
+        self.object_list = list(objects.values())
         #* Loop through all attributes, and set the group attribute with all subjects combined
-        for a in dir(self.objects[0]):
+        for a in dir(self.object_list[0]):
             if not a.startswith('__'):
                 setattr(self,a,np.array([getattr(o,a) for o in self.objects]))
