196407b05d55ed00ae703f7c7088d76452944c9b
SethSullivan <sethsull@udel.edu>
2023-07-06 08:09:15 -0400

    scripts updated to reflect new changes to model object

--- src/loss_functions.py
+++ src/loss_functions.py
@@ -8,6 +8,6 @@ def ape_loss(pred, targets):
     assert pred.shape == targets.shape, "Predictions and Targets must be same shape"
     abs_error     = (np.abs(targets - pred)) / targets
     sum_abs_error = np.sum(abs_error)
-    ape_loss     = (sum_abs_error / targets.size) * 100
-    return ape_loss
+    # ape_loss     = (sum_abs_error / targets.size) * 100
+    return sum_abs_error
 
