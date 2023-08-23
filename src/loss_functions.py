7816d93864cf4b30dfefeff13f5031cf0fc3bfc7
SethSullivan <sethsull@udel.edu>
2023-08-23 08:05:58 -0400

    added in condition if pred and targets are 1d and I need to drop a condition

--- src/loss_functions.py
+++ src/loss_functions.py
@@ -9,8 +9,12 @@ def ape_loss(pred: np.ndarray, targets: np.ndarray, drop_condition_num: int):
     pred = pred.squeeze()
     targets = targets.squeeze()
     if drop_condition_num is not None:
-        pred = np.delete(pred, drop_condition_num, 1)
-        targets = np.delete(targets, drop_condition_num, 1)
+        if pred.ndim>1:
+            pred = np.delete(pred, drop_condition_num, 1)
+            targets = np.delete(targets, drop_condition_num, 1)
+        else:
+            pred = np.delete(pred, drop_condition_num, 0)
+            targets = np.delete(targets, drop_condition_num, 0)
         
     abs_error     = (np.abs(targets - pred)) / targets
     sum_abs_error = np.sum(abs_error)
