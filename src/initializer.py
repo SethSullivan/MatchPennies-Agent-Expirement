6db3098e72f0d00f9ab4b8e21913679fd308fbb9
SethSullivan <sethsull@udel.edu>
2023-10-05 15:33:56 -0400

    update xticklabel titles

--- src/initializer.py
+++ src/initializer.py
@@ -14,21 +14,21 @@ class InitialThangs:
         # Fields pull and pull list
         self.figures_pull_list = []
         self.fields_pull = []
-        with open(self.PATH / "Figures_Pull_List.txt",'r') as pull_file:
+        with open(self.PATH / 'Figures_Pull_List.txt','r') as pull_file:
             self.figures_pull_list = pull_file.read().splitlines()
-        with open(self.PATH/"Fields_Pull.txt",'r') as self.fields_pull:
+        with open(self.PATH/'Fields_Pull.txt','r') as self.fields_pull:
             self.fields_pull = self.fields_pull.read().splitlines()
         self.num_subjects = len(self.figures_pull_list)
 
         if experiment == 'Exp1':
-            self.tp3_title = "1000 [50]"
-            self.tp4_title = "1000 [150]"
-            self.tp5_title = '1100 [50]'
-            self.tp6_title = "1100 [150]"
-            self.tp7_title = "1200 [50]"
-            self.tp8_title = '1200 [150]'
+            self.tp3_title = r'(1000,50)'
+            self.tp4_title = r'(1100,150)'
+            self.tp5_title = r'(1100,50)'
+            self.tp6_title = r'(1100,150)'
+            self.tp7_title = r'(1200,50)'
+            self.tp8_title = r'(1200,150)'
             self.trial_block_titles = [self.tp3_title, self.tp4_title, self.tp5_title,self.tp6_title, self.tp7_title, self.tp8_title]
-            self.xlabel = 'Agent Mean [SD] Movement Onset Time (ms)'
+            self.xlabel = r'Agent $\it{N}\mathbf{(\mu,\sigma)}$ Movement Onset Time (ms)'
             self.num_blocks = len(self.trial_block_titles)
             self.num_trials = 80
             self.num_rows,self.num_cols = 2,3
@@ -39,10 +39,10 @@ class InitialThangs:
             self.f2_collapse_xticklabs = ['50','150']
             
         if experiment == 'Exp2':
-            self.tp3_title = "Win = 1\nIncorrect = 0\nIndecision = 0"
-            self.tp4_title = "Win = 1\nIncorrect = -1\nIndecision = 0"
-            self.tp5_title = "Win = 1\nIncorrect = 0\nIndecision = -1"
-            self.tp6_title = "Win = 1\nIncorrect = -1\nIndecision = -1"
+            self.tp3_title = 'Win = 1\nIncorrect = 0\nIndecision = 0'
+            self.tp4_title = 'Win = 1\nIncorrect = -1\nIndecision = 0'
+            self.tp5_title = 'Win = 1\nIncorrect = 0\nIndecision = -1'
+            self.tp6_title = 'Win = 1\nIncorrect = -1\nIndecision = -1'
             self.trial_block_titles = [self.tp3_title, self.tp4_title, self.tp5_title,self.tp6_title]
             self.num_blocks = len(self.trial_block_titles)
             self.xlabel = 'Feedback Condition'
