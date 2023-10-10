bd9cf6d15e9c716a9f7abe5ff3e0bc8ee9182e7f
SethSullivan <sethsull@udel.edu>
2023-10-10 10:37:02 -0400

    added in colors for means next to eachother and sd next to eachother

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,10 @@
+import data_visualization as dv
+wheel = dv.ColorWheel()
+
 ELECTROMECHANICAL_DELAY = 50
+xticklabel_colors_means = [wheel.rak_blue, wheel.rak_red, wheel.rak_orange,
+                           wheel.dark_blue_hc, wheel.lighten_color(wheel.rak_red,1.5), wheel.burnt_orange,]
+
+xticklabel_colors_sd = [wheel.rak_blue, wheel.dark_blue_hc, 
+                        wheel.rak_red, wheel.lighten_color(wheel.rak_red,1.5),
+                        wheel.rak_orange,wheel.burnt_orange,]
