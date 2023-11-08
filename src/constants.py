45029b457304acd55b9cfe8cbc5723761a3ff1f7
SethSullivan <sethsull@udel.edu>
2023-11-08 08:47:44 -0500

    made paths work for hopefully any computer

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,13 @@
 import data_visualization as dv
 from pathlib import Path
 wheel = dv.ColorWheel()
+def find_parent(path: Path, target_parent: str) -> Path | None:
+    # `path.parents` does not include `path`, so we need to prepend it if it is
+    # to be considered
+    for parent in [path] + list(path.parents):
+        if parent.name == target_parent:
+            return parent
+
 
 ELECTROMECHANICAL_DELAY = 50
 xticklabel_colors_means = [wheel.rak_blue, wheel.rak_red, wheel.rak_orange,
@@ -10,8 +17,8 @@ xticklabel_colors_sd = [wheel.rak_blue, wheel.dark_blue_hc,
                         wheel.rak_red, wheel.lighten_color(wheel.rak_red,1.5),
                         wheel.rak_orange,wheel.burnt_orange,]
 
-MODELS_PATH = Path(r"D:\OneDrive - University of Delaware - o365\Desktop\MatchPennies-Agent-Expirement\results\models")
-MODEL_INPUT_PATH = Path(r"D:\OneDrive - University of Delaware - o365\Desktop\MatchPennies-Agent-Expirement\results\model_inputs")
+MODELS_PATH = find_parent(Path.cwd(),"MatchPennies-Agent-Expirement") / "results" / "models"
+MODEL_INPUT_PATH = find_parent(Path.cwd(),"MatchPennies-Agent-Expirement") / "results" / "model_inputs"
 
 model_input_savenames = ['exp1_rt_median_array.pkl','exp1_rt_sd_array.pkl',
                         'exp1_mt_median_array.pkl','exp1_mt_sd_array.pkl',
