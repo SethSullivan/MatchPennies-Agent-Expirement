e28926247236b6022ad5c94629fe2cad1b7d7074
SethSullivan <sethsull@udel.edu>
2023-11-08 08:48:02 -0500

    added in bootstrap

--- scripts/optimal_model/generate_knockout_models.py
+++ scripts/optimal_model/generate_knockout_models.py
@@ -48,7 +48,7 @@ def create_input_row_dict(model, loss, model_name,):
 
 # * Select experiment you'd like to run
 EXPERIMENT = "Exp1"
-
+BOOTSTRAP = True
 SAVE = True
 
 print(f'Starting up {EXPERIMENT}')
@@ -60,10 +60,20 @@ MODELS_PATH = Path(f"D:\\OneDrive - University of Delaware - o365\\Desktop\\Matc
 LOAD_PATH = Path(f"D:\OneDrive - University of Delaware - o365\Subject_Data\MatchPennies_Agent_{EXPERIMENT}")
 it = InitialThangs(EXPERIMENT)
 
+#* Model input, true parameters
 with open(constants.MODELS_PATH / 'model_input_dict.pkl','rb') as f:
     model_input_dict = dill.load(f)
 true_parameters = {k:np.nanmedian(v) for k,v in model_input_dict.items() if "agent" not in k} 
 
+#* Bootstrap parameters
+with open(constants.MODEL_INPUT_PATH / 'bootstrap_parameter_distribution.pkl','rb') as f:
+    parameter_distribution = dill.load(f)    
+with open(constants.MODEL_INPUT_PATH / 'bootstrap_results.pkl','rb') as f:
+    results = dill.load(f)    
+with open(constants.MODEL_INPUT_PATH / 'participant_ids.pkl','rb') as f:
+    participant_ids = dill.load(f)
+
+#* Particiapnt results, used for loss function 
 with open(constants.MODEL_INPUT_PATH / 'participant_median_movement_onset_time.pkl','rb') as f:
     participant_median_movement_onset_time = dill.load(f)
 with open(constants.MODEL_INPUT_PATH / 'participant_sd_movement_onset_time.pkl','rb') as f:
@@ -78,23 +88,27 @@ with open(constants.MODEL_INPUT_PATH / 'participant_indecisions.pkl','rb') as f:
 # * Set the parameters that change with each model
 # Create keys of what's being changed
 model_dict_keys_sd = ["agent_sd", "rt_sd", "mt_sd", "timing_sd"]
+if BOOTSTRAP:
+    iters = participant_ids.shape[0]
+else:
+    params_dict = {
+        "rt_true":true_parameters['rt'],
+        "rt_expected":true_parameters['rt'],
+        "rt_sd_true":true_parameters['rt_sd'],
+        "rt_sd_expected":true_parameters['rt_sd'],
+        "mt_true":true_parameters['mt'],
+        "mt_expected":true_parameters['mt'],
+        "mt_sd_true":true_parameters['mt_sd'],
+        "mt_sd_expected":true_parameters['mt_sd'],
+        "timing_sd_true":true_parameters['timing_sd'],
+        "timing_sd_expected":true_parameters['timing_sd'],
+        "electromechanical_true":50,
+        "electromechanical_expected":50,
+        "electromechanical_sd_true":50,
+        "electromechanical_sd_expected":50,
+    }
+    param_keys = params_dict.keys()
 
-params_dict = {
-    "rt_true":true_parameters['rt'],
-    "rt_expected":true_parameters['rt'],
-    "rt_sd_true":true_parameters['rt_sd'],
-    "rt_sd_expected":true_parameters['rt_sd'],
-    "mt_true":true_parameters['mt'],
-    "mt_expected":true_parameters['mt'],
-    "mt_sd_true":true_parameters['mt_sd'],
-    "mt_sd_expected":true_parameters['mt_sd'],
-    "timing_sd_true":true_parameters['timing_sd'],
-    "timing_sd_expected":true_parameters['timing_sd'],
-    "electromechanical_true":50,
-    "electromechanical_expected":50,
-    "electromechanical_sd_true":50,
-    "electromechanical_sd_expected":50,
-}
 # * Get targets for model comparisons
 comparison_targets = np.array(
     [
@@ -109,73 +123,94 @@ comparison_targets = np.array(
 c = 0
 input_parameters = []
 descriptive_parameters = []  # Used for saying what changed, as opposed to the actual parameter values
-param_keys = params_dict.keys()
 print("Starting Models...")
 unaccounted_for_parameter = ["rt","rt_sd","mt","mt_sd",
                              "timing_sd","electromechanical","electromechanical_sd"]
-for parameter in unaccounted_for_parameter:
-    #* Set expected parameter to 1, meaning it's not accounted for
-    params_dict[parameter+"_expected"] = 1
-    model_name = f"model{c}_{datetime.now():%Y_%m_%d_%H_%M_%S}"
+input_keys = ["rt","rt_sd","mt","mt_sd","timing_sd",]
 
-    descriptive_parameter_row = {
-        "Model": model_name,
-        "Loss": 0,
-        "Known_RT": params_dict["rt_expected"]==params_dict["rt_true"],
-        "Known_RT_SD": params_dict["rt_sd_expected"]==params_dict["rt_sd_true"],
-        "Known_MT": params_dict["mt_expected"]==params_dict["mt_true"],
-        "Known_MT_SD": params_dict["mt_sd_expected"]==params_dict["mt_sd_true"],
-        "Known_Timing_SD":params_dict["timing_sd_expected"]==params_dict["timing_sd_true"],
-        "Known_Electromechanical": params_dict["electromechanical_expected"]==params_dict["electromechanical_true"],
-        "Known_Electromechanical_SD": params_dict["electromechanical_sd_expected"]==params_dict["electromechanical_sd_true"],
-    }
-    descriptive_parameters.append(descriptive_parameter_row)
-    
-    model = ModelConstructor(
-        experiment=EXPERIMENT,
-        num_blocks=it.num_blocks,
-        num_timesteps=1800,
-        agent_means=np.array([model_input_dict["agent_means"], model_input_dict["agent_means"]])[:, :, np.newaxis],
-        agent_sds=np.array([model_input_dict["agent_sds"], model_input_dict["agent_sds"]])[:, :, np.newaxis],  #!
-        reaction_time=np.array([params_dict["rt_true"], params_dict["rt_expected"]])[:, np.newaxis, np.newaxis],
-        movement_time=np.array([params_dict["mt_true"], params_dict["mt_expected"]])[:, np.newaxis, np.newaxis],
-        reaction_sd=np.array([params_dict["rt_sd_true"], params_dict["rt_sd_expected"]])[
-            :, np.newaxis, np.newaxis
-        ],  #! Reducing these, aka the particiapnt thinks they are more certain than they are
-        movement_sd=np.array([params_dict["mt_sd_true"], params_dict["mt_sd_expected"]])[:, np.newaxis, np.newaxis],
-        timing_sd=np.array([[params_dict["timing_sd_true"]]*it.num_blocks, [params_dict["timing_sd_expected"]]*it.num_blocks])[:, :, np.newaxis],
-        guess_switch_delay=np.array([0,0])[:, np.newaxis, np.newaxis],  # Designed like this for broadcasting reasons
-        guess_switch_sd=np.array([0,0])[:, np.newaxis, np.newaxis],  # This includes electromechanical delay sd and timing sd bc it's straight from data
-        electromechanical_delay=np.array([params_dict["electromechanical_true"], params_dict["electromechanical_expected"]])[:, np.newaxis, np.newaxis],
-        electromechanical_sd=np.array([params_dict["electromechanical_sd_true"], params_dict["electromechanical_sd_expected"]])[:, np.newaxis, np.newaxis],
-        switch_cost_exists=True,
-        expected=True,  #! Should always be True... if the parameter is ground truth, then the two values of the parameter array should be the same
-        win_reward=1.0,
-        incorrect_cost=0.0,  #! These are applied onto the base reward matrix in Optimal Model object
-        indecision_cost=0.0,
-        round_num = 20,
-    )
-    loss = get_loss(
-        model,
-        comparison_targets,
-    )
-    descriptive_parameter_row['Loss'] = loss
-    input_row_dict = create_input_row_dict(model, loss, model_name)
-    input_parameters.append(input_row_dict)
-    c += 1
-    
-    #* Set expected parameter back to true
-    params_dict[parameter+"_expected"] = params_dict[parameter+"_true"]
+for parameter in unaccounted_for_parameter:
+    for i in tqdm(range(iters)):
+        if BOOTSTRAP:
+            parameters = dict(zip(input_keys,parameter_distribution[i,:]))
+            params_dict = params_dict = {
+                            "rt_true":parameters['rt'],
+                            "rt_expected":parameters['rt'],
+                            "rt_sd_true":parameters['rt_sd'],
+                            "rt_sd_expected":parameters['rt_sd'],
+                            "mt_true":parameters['mt'],
+                            "mt_expected":parameters['mt'],
+                            "mt_sd_true":parameters['mt_sd'],
+                            "mt_sd_expected":parameters['mt_sd'],
+                            "timing_sd_true":parameters['timing_sd'],
+                            "timing_sd_expected":parameters['timing_sd'],
+                            "electromechanical_true":50,
+                            "electromechanical_expected":50,
+                            "electromechanical_sd_true":50,
+                            "electromechanical_sd_expected":50,
+                        }
+            
+        #* Set expected parameter to 1, meaning it's not accounted for
+        params_dict[parameter+"_expected"] = 1
+        model_name = f"model{c}_{datetime.now():%Y_%m_%d_%H_%M_%S}"
+
+        descriptive_parameter_row = {
+            "Model": model_name,
+            "Loss": 0,
+            "Known_RT": params_dict["rt_expected"]==params_dict["rt_true"],
+            "Known_RT_SD": params_dict["rt_sd_expected"]==params_dict["rt_sd_true"],
+            "Known_MT": params_dict["mt_expected"]==params_dict["mt_true"],
+            "Known_MT_SD": params_dict["mt_sd_expected"]==params_dict["mt_sd_true"],
+            "Known_Timing_SD":params_dict["timing_sd_expected"]==params_dict["timing_sd_true"],
+            "Known_Electromechanical": params_dict["electromechanical_expected"]==params_dict["electromechanical_true"],
+            "Known_Electromechanical_SD": params_dict["electromechanical_sd_expected"]==params_dict["electromechanical_sd_true"],
+        }
+        descriptive_parameters.append(descriptive_parameter_row)
+        
+        model = ModelConstructor(
+            experiment=EXPERIMENT,
+            num_blocks=it.num_blocks,
+            num_timesteps=1800,
+            agent_means=np.array([model_input_dict["agent_means"], model_input_dict["agent_means"]])[:, :, np.newaxis],
+            agent_sds=np.array([model_input_dict["agent_sds"], model_input_dict["agent_sds"]])[:, :, np.newaxis],  #!
+            reaction_time=np.array([params_dict["rt_true"], params_dict["rt_expected"]])[:, np.newaxis, np.newaxis],
+            movement_time=np.array([params_dict["mt_true"], params_dict["mt_expected"]])[:, np.newaxis, np.newaxis],
+            reaction_sd=np.array([params_dict["rt_sd_true"], params_dict["rt_sd_expected"]])[
+                :, np.newaxis, np.newaxis
+            ],  #! Reducing these, aka the particiapnt thinks they are more certain than they are
+            movement_sd=np.array([params_dict["mt_sd_true"], params_dict["mt_sd_expected"]])[:, np.newaxis, np.newaxis],
+            timing_sd=np.array([[params_dict["timing_sd_true"]]*it.num_blocks, [params_dict["timing_sd_expected"]]*it.num_blocks])[:, :, np.newaxis],
+            guess_switch_delay=np.array([0,0])[:, np.newaxis, np.newaxis],  # Designed like this for broadcasting reasons
+            guess_switch_sd=np.array([0,0])[:, np.newaxis, np.newaxis],  # This includes electromechanical delay sd and timing sd bc it's straight from data
+            electromechanical_delay=np.array([params_dict["electromechanical_true"], params_dict["electromechanical_expected"]])[:, np.newaxis, np.newaxis],
+            electromechanical_sd=np.array([params_dict["electromechanical_sd_true"], params_dict["electromechanical_sd_expected"]])[:, np.newaxis, np.newaxis],
+            switch_cost_exists=True,
+            expected=True,  #! Should always be True... if the parameter is ground truth, then the two values of the parameter array should be the same
+            win_reward=1.0,
+            incorrect_cost=0.0,  #! These are applied onto the base reward matrix in Optimal Model object
+            indecision_cost=0.0,
+            round_num = 20,
+        )
+        loss = get_loss(
+            model,
+            comparison_targets,
+        )
+        descriptive_parameter_row['Loss'] = loss
+        input_row_dict = create_input_row_dict(model, loss, model_name)
+        input_parameters.append(input_row_dict)
+        c += 1
+        
+        #* Set expected parameter back to true
+        params_dict[parameter+"_expected"] = params_dict[parameter+"_true"]
 
 df_inputs = pd.DataFrame(input_parameters)
 df_descriptions = pd.DataFrame(descriptive_parameters)
 if SAVE:
     save_date = datetime.now()
     # * Save the old model table to a new file
-    with open(MODELS_PATH / "knockout_models" / f"{EXPERIMENT}_model_parameters_{save_date:%Y_%m_%d_%H_%M_%S}.pkl", "wb") as f:
+    with open(MODELS_PATH / "knockout_models" / f"{EXPERIMENT}_bootstrapped_model_parameters_{save_date:%Y_%m_%d_%H_%M_%S}.pkl", "wb") as f:
         dill.dump(df_inputs, f)
 
-    with open(MODELS_PATH / "knockout_models" / f"{EXPERIMENT}_model_descriptions_{save_date:%Y_%m_%d_%H_%M_%S}.pkl", "wb") as f:
+    with open(MODELS_PATH / "knockout_models" / f"{EXPERIMENT}_bootstrapped_model_descriptions_{save_date:%Y_%m_%d_%H_%M_%S}.pkl", "wb") as f:
         dill.dump(df_descriptions, f)
 
 print(f"Model generation for {EXPERIMENT} completed")
