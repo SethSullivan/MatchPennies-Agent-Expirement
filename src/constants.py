246c1458148f199ea9463d188d93367512a36e33
SethSullivan <sethsull@udel.edu>
2023-11-21 10:14:53 -0500

    agent_means and sds added

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 import data_visualization as dv
 from pathlib import Path
+import numpy as np
 wheel = dv.ColorWheel()
 def find_parent(path: Path, target_parent: str) -> Path | None:
     # `path.parents` does not include `path`, so we need to prepend it if it is
@@ -25,3 +26,6 @@ model_input_savenames = ['exp1_rt_median_array.pkl','exp1_rt_sd_array.pkl',
                         'exp1_timing_sd_array.pkl','exp1_agent_means.pkl','exp1_agent_sds.pkl']
 # This goes along with parameter distribution so I know which index corresponds to which metric
 input_keys = ["rt","rt_sd","mt","mt_sd","timing_sd",]
+
+agent_means = np.array([1000.0,1000.0,1100.0,1100.0,1200.0,1200.0])
+agent_sds = np.array([50.0,150.0,50.0,150.0,50.0,150.0])
