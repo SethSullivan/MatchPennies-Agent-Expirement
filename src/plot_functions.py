0af8f1ef2669cd0e8606a7882f55eed676107998
SethSullivan <sethsull@udel.edu>
2023-12-12 09:39:39 -0500

    added in my own boxplot function and new plot stats function

--- src/plot_functions.py
+++ src/plot_functions.py
@@ -166,6 +166,107 @@ def make_figure_panel(figsize, inset_size, dpi = 100):
     ax = axmain.inset_axes(inset_size,transform=axmain.transData)
     
     return axmain,ax
+def boxplot(ax, x, data, jitter_data = False, clip_on = True, **kwargs):
+
+    """
+    A function to plot a box plot on a given matplotlib axis with additional options for jittered data. Will omit nans from the data. 
+    Parameters:
+    ax : matplotlib axis object
+        The axis object to plot the box plot on.
+    x : float or int
+        The x position of the box plot on the axis.
+    data : array-like (1D)
+        The data to plot in the box plot.
+    jitter_data : bool, optional
+        If True, adds jittered data to the plot. Default is False.
+    **kwargs : keyword arguments, optional
+    
+    Additional options to customize the plot, including:
+        - linewidth (float): The width of the box plot and whiskers. Default is 1.2.
+        - box_lw (float): The width of the box edges. Default is the linewidth value.
+        - box_width (float): The width of the box. Default is 0.5.
+        - whisker_lw (float): The width of the whiskers. Default is the box_lw value.
+        - color (str): The color of the box plot and whiskers. Default is "#0BB8FD".
+        - noise_scale (float): The scale of the jittered noise added to the data points. Default is 0.06.
+        - data_color (str): The color of the jittered data points. Default is the color value.
+        - include_mean (bool): If True, adds a marker for the mean of the data. Default is False.
+
+        - data_lw (float): The width of the jittered data points. Default is 0.5.
+        - data_size (int): The size of the jittered data points. Default is 40.
+        - data_alpha (float): The alpha value of the jittered data points. Default is 1.
+        - data_zorder (int): The z-order of the jittered data points. Default is 0.
+
+        - mean_size (int): The size of the mean marker. Default is the data_size value.
+        - mean_alpha (float): The alpha value of the mean marker. Default is 1.
+        - mean_color (str): The color of the mean marker. Default is "#727273".
+        - mean_zorder (int): The z-order of the mean marker. Default is 0.
+
+    Returns:
+    ax : matplotlib axis object
+        The axis object with the box plot and optional jittered data added.
+    """
+    if "lw" in kwargs.keys() and "linewidth" in kwargs.keys():
+        raise ValueError("Keyword argument repeated.")
+        
+    linewidth = kwargs.get("linewidth", 1.2)
+    lw = kwargs.get("lw", linewidth)
+    
+    box_lw       = kwargs.get("box_lw", lw)
+    box_width    = kwargs.get("box_width", .5)
+    whisker_lw   = kwargs.get("whisker_lw", box_lw)
+
+    color = kwargs.get("color",   "#0BB8FD")
+
+    #box properties
+    box_props   = {"facecolor": "none", "edgecolor" : color, "linewidth": box_lw, "alpha": 1}
+
+    #whisker properties
+    whisker_props = {"linewidth" : whisker_lw, "color": color}
+
+    #cap properties
+    cap_props = {"linewidth" : whisker_lw, "color": color}
+
+    #median properties
+    median_props = {"linewidth" : whisker_lw, "color": color}
+
+    '''Make Box Plots'''
+    ax.set_facecolor("none")
+    
+    data = np.array(data)
+    filtered_data = data[~np.isnan(data)]
+    #Make Box
+    bp = ax.boxplot([filtered_data],   positions = [x], patch_artist = True,  showfliers = False, 
+                boxprops = box_props  , whiskerprops = whisker_props,
+                capprops = cap_props, medianprops = median_props, widths = box_width)
+    
+    #Add jittered data
+    if jitter_data:
+        noise_scale = kwargs.get("noise_scale", .06)
+        data_color = kwargs.get("data_color", color)
+        include_mean = kwargs.get("include_mean", False)
+        
+        data_lw = kwargs.get("data_lw", .5)
+        data_size = kwargs.get("data_size", 40)
+        data_alpha = kwargs.get("data_alpha", 1)
+        data_zorder = kwargs.get("data_zorder", 0)
+        
+        mean_size = kwargs.get("mean_size", data_size)
+        mean_alpha = kwargs.get("mean_alpha", 1)
+        mean_color = kwargs.get("mean_color", '#727273')
+        mean_zorder = kwargs.get("mean_zorder", 0)
+
+        noise = np.random.normal(0, noise_scale, len(filtered_data))
+        
+        ax.scatter(x + noise, filtered_data,
+                s = data_size, facecolors = 'none',
+               edgecolors=data_color, alpha = data_alpha, lw = data_lw, zorder = data_zorder, clip_on = clip_on)
+            
+        if include_mean:
+            ax.scatter(x, np.nanmean(filtered_data) ,
+                        s = data_size, facecolors = mean_color,
+                       edgecolors=mean_color, alpha = mean_alpha, lw = data_lw, zorder = mean_zorder, clip_on = clip_on)
+        
+    return ax
 
 def multi_boxplot(ax, data, xlocs, **kwargs):
     """
@@ -344,7 +445,24 @@ def multiple_models_boxplot(ax,data,model_data,labels,
     dv.legend(ax,legend_labels,legend_colors,ls = legend_linestyles,loc=pk.legend_loc,
               fontsize=pk.legend_fontsize, ncol=ncol, columnspacing=1,linewidth=pk.legend_linewidth)
     
-    
+def plot_stats_v2(ax, pvals:list[dict],cles, combos:list[str], 
+               ypos:list[int], xlocs:list[list[float,float]], 
+               show_effectsize=False, **kwargs):
+    '''
+    stacked: 
+    fontsize: 
+    h: 
+    lw: 
+    '''
+        
+    for i in range(len(combos)):
+        if show_effectsize:
+            kwargs.update({'cles':cles.iloc[0][combos[i]]})
+        else:
+            kwargs.update({'cles':None})
+        dv.stat_annotation(ax,xlocs[i][0],xlocs[i][1],y=ypos[i],
+                           p_val= pvals.iloc[0][combos[i]], **kwargs)
+
 def plot_stats(ax, statistics:list[dict], combos:list[str], 
                ypos:list[int], xpositions:list[list[float,float]], 
                show_effectsize=False, **kwargs):
@@ -395,7 +513,31 @@ def plot_models(ax, xlocs, data, line_colors, linestyles, bw,
     if legend:
         dv.legend(ax,legend_labels,legend_colors,ls = legend_linestyles,loc=legend_loc,
                 fontsize=legend_fontsize, ncol=ncol, columnspacing=columnspacing, linewidth=legend_linewidth)
+
+def exp2_reaction_plots(ax, metric, pvals, cles, 
+                        ylocs, statline_y, shift_statlines, 
+                        h, colors =[wheel.rak_red, wheel.rak_blue, wheel.dark_red, wheel.dark_blue]
+    ):
     
+    for i in range(4):
+        dv.boxplot(metric[:,i],x_pos = i,ax=ax,box_lw=3, linewidth=3,whisker_lw=3, color = colors[i])
+    dv.jitter_array(ax =ax, x_positions = xlocs,
+                    data = metric.T,
+                    circle_size = 75,include_mean=False,
+                    data_color = colors, data_edge_color = wheel.light_grey, circle_lw=1.)
+    np.random.seed(2)
+    xlocs = np.arange(0,5,1)
+    combos = ['01','02','13','23',]
+    for i,c in enumerate(combos):
+        a = int(c[0])
+        b = int(c[1])
+        if pvals[c]<0.05:
+            dv.stat_annotation(ax,xlocs[a],xlocs[b],statline_y,p_val=pvals[c],cles=cles[c],
+                            fontsize=12,h=h, lw=1.25)
+            statline_y += shift_statlines[i]
+    
+
+   
 #! Legacy for Optimal_Stopping_Model_With_Data_Group
 #! v2 is a much more concise implementation and is better
 def multiple_models_boxplot_old(ax,data,show_boxplot=True,
