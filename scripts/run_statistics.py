1d0d9254976b147b1dbe6a9a077bb6a7e56ecc6e
SethSullivan <sethsull@udel.edu>
2024-01-03 09:15:48 -0500

    lots of updates

--- scripts/run_statistics.py
+++ scripts/run_statistics.py
@@ -5,13 +5,14 @@ import dill
 from itertools import combinations
 from pathlib import Path
 
+from initializer import InitialThangs
 import analysis_utilities as au
 
 def df_to_array(df_col, num_subjects, num_blocks):
     ans = np.array(df_col).reshape(num_subjects, num_blocks)
     return ans
 
-def collapse_across(arr, conditions:str):
+def exp1_collapse_across(arr, conditions:str):
     '''
     conditions: 'means' or 'sds'
     '''
@@ -26,6 +27,24 @@ def collapse_across(arr, conditions:str):
         new_arr = np.vstack((
             np.concatenate((arr[:,0], arr[:,2], arr[:,4])),
             np.concatenate((arr[:,1], arr[:,3], arr[:,5]))
+            )
+        ).T
+    return new_arr
+
+def exp2_collapse_across(arr, conditions:str):
+    '''
+    conditions: 'mixed_only' or 'react_guess'
+    '''
+    if conditions == "mixed_only":
+        new_arr = np.vstack((
+            np.concatenate((arr[:,0], arr[:,1])),
+            np.concatenate((arr[:,2], arr[:,3])),
+            )
+        ).T
+    elif conditions == "react_guess":
+        new_arr = np.vstack((
+            np.concatenate((arr[:,0], arr[:,2])),
+            np.concatenate((arr[:,1], arr[:,3])),
             )
         ).T
     return new_arr
@@ -93,11 +112,6 @@ def pairwise_bootstrap(data, combos, alternative ="two-sided", M=1e6, test="mean
                                             test=test)})
         cles1.update({combo:au.cles(data[:, i], data[:, j], paired=True)}) 
         cles2.update({combo:au.cles(data[:, j], data[:, i], paired=True)}) 
-
-    # Create array and do holm bonferroni
-    # print(pvals)
-    pvals_corrected = au.holmbonferroni_correction(np.array(list(pvals.values())))
-    pvals_corrected_dict = dict(zip(combos,pvals_corrected))
     
     cles_dict = {}
     for (k1,v1),(k2,v2) in zip(cles1.items(),cles2.items()):
@@ -106,18 +120,35 @@ def pairwise_bootstrap(data, combos, alternative ="two-sided", M=1e6, test="mean
         else:
             cles_dict.update({k2:v2})
     
-    return pvals_corrected_dict, cles_dict
+    return pvals, cles_dict
 
-with open(r'D:\OneDrive - University of Delaware - o365\Desktop\MatchPennies-Agent-Expirement\results\participant_data\Exp1_stats_df.pkl', "rb") as f:
+
+EXPERIMENT = "Exp2"
+print(EXPERIMENT)
+with open(rf'D:\OneDrive - University of Delaware - o365\Desktop\MatchPennies-Agent-Expirement\results\participant_data\{EXPERIMENT}_stats_df.pkl', "rb") as f:
     df = dill.load(f)
+it = InitialThangs(EXPERIMENT)
+if EXPERIMENT == "Exp1":
+    column_names = ["Median_Movement_Onset_Time", "SD_Movement_Onset_Time","Wins","Incorrects","Indecisions"]
+    condition_nums = ["0", "1", "2", "3", "4", "5"]
+    # Only care about mean comparisons, and then 01 and 12 for the collapse
+    all_combos = ["01", "12", '02', '04', '13', '15', '24', '35']
+    collapse_across = exp1_collapse_across
+    collapse_arg = "means"
+    collapsed_combos = ["01","02","12"]
+    all_combo_slice = slice(2,None)
+elif EXPERIMENT == "Exp2":
+    column_names = ["Reaction_Time_Median", "Reaction_Time_SD","Reaction_Time_IQR"]
+    condition_nums = ["0", "1", "2", "3",]
+    all_combos = ["01","02","13","23"]
+    collapse_across = exp2_collapse_across
+    collapse_arg = "react_guess"
+    collapsed_combos = ["01"]
+    all_combo_slice = slice(0,None)
     
-    
-column_names = ["Median_Movement_Onset_Time", "SD_Movement_Onset_Time","Wins","Incorrects","Indecisions"]
-condition_nums = ["0", "1", "2", "3", "4", "5"]
-# Only care about mean comparisons, and then 01 and 12 for the collapse
-all_combos = ["01", "12", '02', '04', '13', '15', '24', '35'] # Check parity so I can get onyl the 024 and 135 comparisons
 anova_df = pd.DataFrame()
 pvals_df = pd.DataFrame(columns = ["Metric", "Collapsed"] + all_combos)
+pvals_uncorr_df = pd.DataFrame(columns = ["Metric", "Collapsed"] + all_combos)
 cles_df = pd.DataFrame(columns = ["Metric", "Collapsed"] + all_combos)
 
 for i,colname in enumerate(column_names):
@@ -133,17 +164,17 @@ for i,colname in enumerate(column_names):
     
     
     data_array = df_to_array(df[colname],
-                             num_subjects=20, 
-                             num_blocks=6)
+                             num_subjects=it.num_subjects, 
+                             num_blocks=it.num_blocks)
     if anova["p-GG-corr"][2] < 0.05:
         print("Significant interaction, doing pairwise bootstraps for each condition...")
-        new_combos = all_combos[2:]
-        collapsed=False
+        new_combos = all_combos[all_combo_slice] # Only want to pairwise within same sds, so want 02,04,24, and 13,15,35
+        collapsed=np.nan
     else: 
         print("Non-significant interaction, collapsing across conditions...")
-        data_array = collapse_across(data_array,"means")
-        new_combos = ["01","02","12"]
-        collapsed = True
+        data_array = collapse_across(data_array,collapse_arg)
+        new_combos = collapsed_combos
+        collapsed = collapse_arg
     
     pvals,cles = pairwise_bootstrap(
         data = data_array,
@@ -151,17 +182,26 @@ for i,colname in enumerate(column_names):
         alternative="two-sided",
         test='mean'
     )
+    pvals_corrected = au.holmbonferroni_correction(list(pvals.values()))
+    pvals_corrected = dict(zip(pvals.keys(),pvals_corrected))
     d1 = {"Metric":colname, "Collapsed":collapsed}
-    df_row_pvals = d1 | pvals 
+    df_row_pvals_uncorrected = d1 | pvals
+    df_row_pvals = d1 | pvals_corrected 
     df_row_cles = d1 | cles
+    pvals_uncorr_df = pd.concat([pvals_uncorr_df, pd.DataFrame(df_row_pvals_uncorrected,index=[i])]).fillna(np.nan)
     pvals_df = pd.concat([pvals_df, pd.DataFrame(df_row_pvals,index=[i])]).fillna(np.nan)
     cles_df = pd.concat([cles_df, pd.DataFrame(df_row_cles,index=[i])]).fillna(np.nan)
+
 SAVE_PATH = Path(r"D:\OneDrive - University of Delaware - o365\Desktop\MatchPennies-Agent-Expirement\results\participant_data")
-with open(SAVE_PATH / "Exp1_pvals_df.pkl","wb") as f:
+
+with open(SAVE_PATH / f"{EXPERIMENT}_pvals_uncorrected_df.pkl","wb") as f:
+    dill.dump(pvals_uncorr_df, f)
+    
+with open(SAVE_PATH / f"{EXPERIMENT}_pvals_df.pkl","wb") as f:
     dill.dump(pvals_df, f)
     
-with open(SAVE_PATH / "Exp1_cles_df.pkl","wb") as f:
+with open(SAVE_PATH / f"{EXPERIMENT}_cles_df.pkl","wb") as f:
     dill.dump(cles_df, f)
     
-with open(SAVE_PATH / "Exp1_anova_df.pkl","wb") as f:
+with open(SAVE_PATH / f"{EXPERIMENT}_anova_df.pkl","wb") as f:
     dill.dump(anova_df, f)
