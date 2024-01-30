18528dcc77777f3ccc3288a7cd69d78aeffe21a9
SethSullivan <sethsull@udel.edu>
2024-01-30 16:09:43 -0500

    now getting trial by trial dataframe along with summary statistics

--- scripts/generate_df_for_stats.py
+++ scripts/generate_df_for_stats.py
@@ -8,7 +8,7 @@ import read_data_functions as rdf
 
 
 
-def generate_exp1_stats_dataframe(group, EXPERIMENT="Exp1", DROP_SUBJECT_NUM=None, num_trials=80):
+def generate_exp1_summary_dataframe(group, EXPERIMENT="Exp1", DROP_SUBJECT_NUM=None, num_trials=80):
     def perc(metric,):
         return (metric / num_trials) * 100
 
@@ -18,6 +18,7 @@ def generate_exp1_stats_dataframe(group, EXPERIMENT="Exp1", DROP_SUBJECT_NUM=Non
     incorrects = perc(group.score_metrics.score_metric("incorrects")).flatten().tolist()
     correct_decisions = perc(group.movement_metrics.correct_initial_decisions).flatten().tolist()
     median_movement_onset_time = np.nanmedian(group.movement_metrics.movement_onset_times("task"), axis=2).flatten().tolist()
+    mean_movement_onset_time = np.nanmean(group.movement_metrics.movement_onset_times("task"), axis=2).flatten().tolist()
     q1_median_movement_onset_time = np.nanquantile(group.movement_metrics.movement_onset_times("task"), 0.25, axis=2).flatten().tolist()
     q3_median_movement_onset_time = np.nanquantile(group.movement_metrics.movement_onset_times("task"), 0.75, axis=2).flatten().tolist()
     movement_onset_time_sd = np.nanstd(group.movement_metrics.movement_onset_times("task"), axis=2).flatten().tolist()
@@ -37,17 +38,11 @@ def generate_exp1_stats_dataframe(group, EXPERIMENT="Exp1", DROP_SUBJECT_NUM=Non
         factor1 = np.tile(["0 Inc", "-1 Inc", "0 Inc", "-1 Inc"], it.num_subjects)
         factor2 = np.tile(["0 Ind", "0 Ind", "-1 Ind", "-1 Ind"], it.num_subjects)
         points = group.score_metrics.exp2_points_scored.flatten().tolist()
-        decision_time_difference_punish_incorrects = (
-            np.nanmedian(group.movement_metrics.movement_onset_times("task"), axis=2)[0]
-            - np.nanmedian(group.movement_metrics.movement_onset_times("task"), axis=2)[1]
-        )
-        decision_time_difference_punish_indecisions = (
-            np.nanmedian(group.movement_metrics.movement_onset_times("task"), axis=2)[0]
-            - np.nanmedian(group.movement_metrics.movement_onset_times("task"), axis=2)[2]
-        )
+
     df_metrics = pd.DataFrame(
         np.array(
             [
+                mean_movement_onset_time,
                 median_movement_onset_time,
                 median_movement_time,
                 wins,
@@ -65,6 +60,7 @@ def generate_exp1_stats_dataframe(group, EXPERIMENT="Exp1", DROP_SUBJECT_NUM=Non
             ]
         ).T,
         columns=[
+            "Mean_Movement_Onset_Time",
             "Median_Movement_Onset_Time",
             "Median_Movement_Time",
             "Wins",
@@ -91,26 +87,103 @@ def generate_exp1_stats_dataframe(group, EXPERIMENT="Exp1", DROP_SUBJECT_NUM=Non
     print(f"!! DROPPING SUBJECT {DROP_SUBJECT_NUM} !! ")
     return df
 
-def generate_exp2_stats_dataframe(group2):
+def generate_exp1_trial_dataframe(group, EXPERIMENT="Exp1", DROP_SUBJECT_NUM=None, num_trials=80):
+    '''
+    Long format data of each subjects data for every trial (so not collapsed into a mean across trials)
+    
+    '''
+    def perc(metric,):
+        return (metric / num_trials) * 100
+    
+    def define_react_guess_decisions(
+                                     player_movement_onset_times:np.ndarray, 
+                                     agent_movement_onset_times:np.ndarray, 
+                                     incorrect_mask, 
+                                     subtract_off_reaction:int=25):
+        mean_reaction_times = np.nanmedian(group.movement_metrics.reaction_times, axis=1)
+        temp = mean_reaction_times - subtract_off_reaction
+        adjusted_reaction_times = np.tile(temp,(it.num_blocks,num_trials))
+        print(adjusted_reaction_times.shape)
+        guess_mask =  (
+            ((player_movement_onset_times - agent_movement_onset_times) <= adjusted_reaction_times) | (incorrect_mask)
+        )
+        return guess_mask, ~guess_mask
+    
+
+    it = InitialThangs(EXPERIMENT)
+    win = group.score_metrics.win_mask.flatten().astype(int)
+    indecision = group.score_metrics.indecision_mask.flatten().astype(int)
+    incorrect = group.score_metrics.incorrect_mask.flatten().astype(int)
+    movement_onset_times = group.movement_metrics.movement_onset_times("task").flatten()
+    agent_movement_onset_times = group.raw_data.agent_task_leave_time.flatten()
+    target_reach_times = group.movement_metrics.target_reach_times("task").flatten()
+    movement_times = group.movement_metrics.movement_times("task").flatten()
+    reaction_times = np.repeat(np.nanmedian(group.movement_metrics.reaction_times - 25, axis=1), it.num_blocks*it.num_trials) #! Subtracting off 25 ms
+    subject_number = np.repeat(np.arange(1, it.num_subjects + 1, 1, dtype=int), it.num_blocks*it.num_trials)
+    condition = np.tile(np.repeat(np.arange(1, it.num_blocks + 1, 1, dtype=int),it.num_trials), it.num_subjects)
+    trial_number = np.tile(np.arange(1,it.num_trials + 1, 1, dtype=int), it.num_subjects*it.num_blocks)
+    # alt_condition = np.tile(['1000 (50)','1000 (150)','1100 (50)','1100 (150)', '1200 (50)', '1200 (150)'],it.num_subjects).tolist()
+    if EXPERIMENT == "Exp1":
+        factor1 = np.repeat(["1000", "1000", "1100", "1100", "1200", "1200"], it.num_subjects*it.num_trials)
+        factor2 = np.repeat(["50","50","50", "150", "150","150"], it.num_subjects*it.num_trials)
+        points = np.full_like(win, 0)
+    else:
+        factor1 = np.tile(["0 Inc", "-1 Inc", "0 Inc", "-1 Inc"], it.num_subjects)
+        factor2 = np.tile(["0 Ind", "0 Ind", "-1 Ind", "-1 Ind"], it.num_subjects)
+        points = group.score_metrics.exp2_points_scored.flatten().tolist()
+
+    df_metrics = pd.DataFrame(
+        np.array(
+            [
+                win,
+                indecision,
+                incorrect,
+                movement_onset_times,
+                agent_movement_onset_times,
+                target_reach_times,
+                movement_times,
+                reaction_times,
+            ]
+        ).T,
+        columns=[
+            "win",
+            "indecision",
+            "incorrect",
+            "player_movement_onset_time",
+            "agent_movement_onset_time",
+            "target_reach_time",
+            "movement_time",
+            "reaction_time"
+        ],
+    )
+    print(np.vstack((subject_number, condition, factor1, factor2)))
+    df_conditions = pd.DataFrame(np.vstack((subject_number, condition, trial_number, factor1, factor2)).T, 
+                                 columns=["Subject", "Condition","Trial","Factor_1", "Factor_2"])
+    df = pd.concat([df_conditions, df_metrics], axis=1)
+    df['reaction_decision'] = df["player_movement_onset_time"]-df["agent_movement_onset_time"]>=df['reaction_time']
+    df['guess_decision'] = df["player_movement_onset_time"]-df["agent_movement_onset_time"]<df['reaction_time']
+    return df
+
+def generate_exp2_summary_dataframe(group2):
     it2 = InitialThangs("Exp2")
 
-    react_mixed_median  = np.nanmedian(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','mixed'),axis=1)
-    react_only_median   = np.nanmedian(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','only'),axis=1)
-    guess_mixed_median = np.nanmedian(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','mixed'),axis=1)
-    guess_only_median  = np.nanmedian(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','only'),axis=1)
+    react_mixed_median   = np.nanmedian(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','mixed'),axis=1)
+    react_only_median    = np.nanmedian(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','only'),axis=1)
+    guess_mixed_median   = np.nanmedian(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','mixed'),axis=1)
+    guess_only_median    = np.nanmedian(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','only'),axis=1)
     reaction_time_median = np.vstack((react_mixed_median,guess_mixed_median,react_only_median,guess_only_median)).T.flatten()
 
-    react_mixed_sd  = np.nanstd(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','mixed'),axis=1)
-    react_only_sd   = np.nanstd(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','only'),axis=1)
-    guess_mixed_sd = np.nanstd(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','mixed'),axis=1)
-    guess_only_sd  = np.nanstd(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','only'),axis=1)
-    reaction_time_sd    = np.vstack((react_mixed_sd,guess_mixed_sd,react_only_sd,guess_only_sd)).T.flatten()
+    react_mixed_sd   = np.nanstd(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','mixed'),axis=1)
+    react_only_sd    = np.nanstd(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','only'),axis=1)
+    guess_mixed_sd   = np.nanstd(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','mixed'),axis=1)
+    guess_only_sd    = np.nanstd(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','only'),axis=1)
+    reaction_time_sd = np.vstack((react_mixed_sd,guess_mixed_sd,react_only_sd,guess_only_sd)).T.flatten()
 
-    react_mixed_iqr  = iqr(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','mixed'),axis=1,nan_policy='omit')
-    react_only_iqr   = iqr(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','only'),axis=1,nan_policy='omit')
-    guess_mixed_iqr = iqr(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','mixed'),axis=1,nan_policy='omit')
-    guess_only_iqr  = iqr(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','only'),axis=1,nan_policy='omit')
-    reaction_time_iqr    = np.vstack((react_mixed_iqr,guess_mixed_iqr,react_only_iqr,guess_only_iqr)).T.flatten()
+    react_mixed_iqr   = iqr(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','mixed'),axis=1,nan_policy='omit')
+    react_only_iqr    = iqr(group2.movement_metrics.exp2_react_guess_reaction_time_split('react','only'),axis=1,nan_policy='omit')
+    guess_mixed_iqr   = iqr(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','mixed'),axis=1,nan_policy='omit')
+    guess_only_iqr    = iqr(group2.movement_metrics.exp2_react_guess_reaction_time_split('guess','only'),axis=1,nan_policy='omit')
+    reaction_time_iqr = np.vstack((react_mixed_iqr,guess_mixed_iqr,react_only_iqr,guess_only_iqr)).T.flatten()
 
     subject_number = np.repeat(np.arange(1, it2.num_subjects + 1, 1, dtype=int), 4)
     condition = np.tile(np.arange(1, 4 + 1, 1, dtype=int), it2.num_subjects)    
@@ -126,7 +199,7 @@ def generate_exp2_stats_dataframe(group2):
     return df2
     
 
-def generate_exp2_reaction_dataframe(group):
+def generate_exp2_trial_dataframe(group):
     '''
     THIS creates the raw data df, not the summary (like medians, sd, iqr)
     '''
@@ -170,12 +243,17 @@ else:
     if group2.exp_info.experiment != "Exp2":  # This means i changed experiment and need to run again
         group2 = rdf.generate_subject_object_v3("Exp2", "All Trials", movement_metric_type='velocity')
 
-df1 = generate_exp1_stats_dataframe(group, EXPERIMENT, DROP_SUBJECT_NUM=None)
-df2 = generate_exp2_stats_dataframe(group2)
+df1 = generate_exp1_summary_dataframe(group, EXPERIMENT, DROP_SUBJECT_NUM=None)
+df2 = generate_exp2_summary_dataframe(group2)
 
 # Create raw dataframe if I want to use later for side analyses
-generate_exp2_reaction_dataframe(group2)
-with open(SAVE_PATH / f"{EXPERIMENT}_stats_df.pkl", "wb") as f:
+df3 = generate_exp1_trial_dataframe(group)
+df4 = generate_exp2_trial_dataframe(group2)
+with open(SAVE_PATH / f"Exp1_summary_data_df.pkl", "wb") as f:
     dill.dump(df1,f)
-with open(SAVE_PATH / f"Exp2_stats_df.pkl", "wb") as f:
+with open(SAVE_PATH / f"Exp2_summary_data_df.pkl", "wb") as f:
     dill.dump(df2,f)
+with open(SAVE_PATH / f"Exp1_trial_data_df.pkl", "wb") as f:
+    dill.dump(df3,f)
+with open(SAVE_PATH / f"Exp2_trial_data_df.pkl", "wb") as f:
+    dill.dump(df4,f)
