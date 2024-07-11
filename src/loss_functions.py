587f82fb7c529a5ba1def15fdd43708ff0f110ff
SethSullivan <sethsull@udel.edu>
2024-07-11 15:26:16 -0400

    addd mse loss function

--- src/loss_functions.py
+++ src/loss_functions.py
@@ -2,7 +2,8 @@ import numpy as np
 
 def ape_loss(pred: np.ndarray, targets: np.ndarray, ):
     '''
-    12/07/23 -   Absolute Percent Error
+    12/07/23 -   Absolute Percent Error, ended up just doing the sum of the absolute error
+    07/10/24 - Used this primarily, but going to try mean squared error 
     
     '''
     if isinstance(pred, list):
@@ -12,16 +13,23 @@ def ape_loss(pred: np.ndarray, targets: np.ndarray, ):
     assert pred.shape == targets.shape, "Predictions and Targets must be same shape"
     pred = pred.squeeze()
     targets = targets.squeeze()
-    # if drop_condition_num is not None:
-    #     if pred.ndim>1:
-    #         pred = np.delete(pred, drop_condition_num, 1)
-    #         targets = np.delete(targets, drop_condition_num, 1)
-    #     else:
-    #         pred = np.delete(pred, drop_condition_num, 0)
-    #         targets = np.delete(targets, drop_condition_num, 0)
-        
+    
     abs_error     = np.abs((targets - pred) / targets)
     sum_abs_error = np.sum(abs_error)
     # ape_loss     = (sum_abs_error / targets.size) * 100
     return sum_abs_error
 
+def mse_loss(predictions, targets):
+    if isinstance(predictions, list):
+        predictions = np.array(predictions)
+    if isinstance(targets, list):
+        targets = np.array(targets)
+        
+    assert predictions.shape == targets.shape, "Predictions and Targets must be same shape"
+    
+    predictions = predictions.squeeze()
+    targets = targets.squeeze()
+    
+    error = np.mean((predictions - targets)**2)
+    
+    return error 
