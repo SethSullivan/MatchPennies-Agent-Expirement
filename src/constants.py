57ceebf18997d43697965ff101aa961ac715fc9c
SethSullivan <sethsull@udel.edu>
2024-07-23 12:34:59 -0400

    xticklabels only mean, dropping variance terminology in v2

--- src/constants.py
+++ src/constants.py
@@ -42,6 +42,8 @@ xticklabels_sd = [r"Early Mean"+"\n"+r"Low Variance",
                r"Middle Mean"+"\n"+r"High Variance", 
                r"Late Mean"+"\n"+r"Low Variance", 
                r"Late Mean"+"\n"+r"High Variance"]
+
+xticklabels_only_mean = ["Early Mean", "Middle Mean", "Late Mean"]
 MODELS_PATH = find_parent(Path.cwd(),"MatchPennies-Agent-Expirement") / "results" / "models"
 MODEL_INPUT_PATH = find_parent(Path.cwd(),"MatchPennies-Agent-Expirement") / "results" / "model_inputs"
 
