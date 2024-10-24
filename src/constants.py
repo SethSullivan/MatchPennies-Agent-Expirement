ee0c3bf525370ae8cbb1725fbe858e657aa710ea
SethSullivan <sethsull@udel.edu>
2024-10-24 16:26:44 -0400

    added dark_colors

--- src/constants.py
+++ src/constants.py
@@ -65,6 +65,7 @@ cadet_grey      = "#91A3B0"
 marengo         = "#4C5866"
 pebble          = "#333333"
 model_colors = [cadet_grey, wheel.lighten_color(pebble,0.75), wheel.black]
+model_colors_dark = [cadet_grey, wheel.lighten_color(pebble,0.75), wheel.white]
 model_names = ["No Switch Time Model", "Full Switch Time Model", "Partial Switch Time Model"]
 
 stimulus_color = wheel.lighten_color(wheel.yellow, 1.75)
